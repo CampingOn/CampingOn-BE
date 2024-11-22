e2a24c07703b1cc6f88c2036160adb61b677eff5
[CLOUD4] 박유찬 <pychan0306@gmail.com>
2024-11-22 19:38:03 +0900

    Feat/#20 CampRepository& CampService 구현

--- src/main/java/site/campingon/campingon/camp/repository/CampRepository.java
+++ src/main/java/site/campingon/campingon/camp/repository/CampRepository.java
@@ -1,5 +1,20 @@
 package site.campingon.campingon.camp.repository;
 
-public class CampRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import site.campingon.campingon.camp.entity.Camp;
 
+import java.util.List;
+
+@Repository
+public interface CampRepository extends JpaRepository<Camp, Long> {
+    List<Camp> findRecommendedCampsByUserId(Long userId, int size);
+
+    boolean isLikedByUser(Long id, Long userId);
+
+    Page<Camp> findPopularCamps(PageRequest pageRequest);
+
+    Page<Camp> searchCamps(String keyword, String location, PageRequest pageRequest);
 }
